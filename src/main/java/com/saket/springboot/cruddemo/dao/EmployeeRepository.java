918ed9398afcec3376255f36d6b124c823ba6785
Saket Sinha <saket.sinha@active.ai>
2020-01-20 18:47:22 +0530

    Spring Data Rest

---
+++ src/main/java/com/saket/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.saket.springboot.cruddemo.dao;
+
+import com.saket.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+}
---
+++ src/test/java/com/saket/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saket.springboot.cruddemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
